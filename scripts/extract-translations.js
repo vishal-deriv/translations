5b0d5f549100cee9920751bb821d6adaf4aa6e7a
amir ali <amir.ali@regentmarkets.com>
2024-04-29 11:56:47 +0500

    feat: add extract translation cli

--- scripts/extract-translations.js
+++ scripts/extract-translations.js
@@ -1,44 +1,89 @@
+#!/usr/bin/env node
+
+/* eslint-disable */
+const path = require("path");
+const program = require("commander");
 const crc32 = require("crc-32").str;
-const glob = require("glob");
 const fs = require("fs");
+const glob = require("glob");
+const DOMParser = require("@xmldom/xmldom").DOMParser;
 
 const getRegexPattern = () =>
   /(i18n_default_text={?|localize\()\s*(['"])\s*(.*?)(?<!\\)\2\s*/gs;
 
 const getStringsFromInput = (input, i18n_marker = getRegexPattern()) => {
   const messages = [];
+  let result = i18n_marker.exec(input);
 
-  let continue_loop = true;
-  while (continue_loop) {
-    const result = i18n_marker.exec(input);
-    if (result == null) continue_loop = false;
-    else {
-      const extracted = result[3];
-      // Replace escape characters.
-      messages.push(extracted.replace(/\\/g, ""));
-    }
+  while (result !== null) {
+    const extracted = result[3];
+    // Replace escape characters.
+    messages.push(extracted.replace(/\\/g, ""));
+    result = i18n_marker.exec(input);
   }
 
   return messages;
 };
 
+const getStringsFromXmlFile = (input) => {
+  const messages = [];
+  const parsed_xml = new DOMParser().parseFromString(input, "application/xml");
+  const el_categories = parsed_xml.getElementsByTagName("category");
+
+  Array.from(el_categories).forEach((el_category) => {
+    const name = el_category.getAttribute("name");
+    const description = el_category.getAttribute("description");
+
+    if (name) messages.push(name);
+    if (description) messages.push(description);
+  });
+
+  return messages;
+};
+
 const getTranslatableFiles = () => {
-  const globs = ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"];
+  const packages_with_translations = [
+    "account",
+    "appstore",
+    "cashier",
+    "bot-web-ui",
+    "core",
+    "cfd",
+    "trader",
+    "bot-skeleton",
+    "reports",
+    "shared",
+  ];
+  const globs = ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx", "**/xml/*.xml"];
   const file_paths = [];
 
-  for (let j = 0; j < globs.length; j++) {
-    let files_found = glob.sync(
-      `${process.env.GITHUB_WORKSPACE}/src/${globs[j]}`
-    );
-    files_found = files_found.filter(
-      (file_path) => file_path.indexOf("__tests__") === -1
-    );
-    file_paths.push(...files_found);
+  for (let i = 0; i < packages_with_translations.length; i++) {
+    for (let j = 0; j < globs.length; j++) {
+      let files_found = glob.sync(
+        `/${packages_with_translations[i]}/src/${globs[j]}`,
+        {
+          root: path.resolve(__dirname, "../.."), // deriv-app/packages/
+        }
+      );
+      files_found = files_found.filter(
+        (file_path) => file_path.indexOf("__tests__") === -1
+      );
+      file_paths.push(...files_found);
+    }
   }
 
   return file_paths;
 };
 
+program
+  .version("0.1.0")
+  .description("Build translation source.")
+  .option("-v, --verbose", "Displays the list of paths to be compiled")
+  .parse(process.argv);
+
+/** *********************************************
+ * Common
+ */
 const getKeyHash = (string) => crc32(string);
 
 /** **********************************************
@@ -52,11 +97,19 @@ const getKeyHash = (string) => crc32(string);
 
     // Iterate over files and extract all strings from the i18n marker
     for (let i = 0; i < file_paths.length; i++) {
-      console.log(file_paths[i]);
+      const file_path = file_paths[i];
 
       try {
-        const file = fs.readFileSync(file_paths[i], "utf8");
-        messages.push(...getStringsFromInput(file));
+        if (program.verbose) {
+          console.log(file_path);
+        }
+
+        const file = fs.readFileSync(file_path, "utf8");
+        messages.push(
+          ...(file_path.endsWith("xml")
+            ? getStringsFromXmlFile(file)
+            : getStringsFromInput(file))
+        );
       } catch (e) {
         console.log(e);
       }
@@ -69,7 +122,7 @@ const getKeyHash = (string) => crc32(string);
 
     // Add to messages.json
     fs.writeFileSync(
-      `${process.env.GITHUB_WORKSPACE}/crowdin/messages.json`,
+      path.resolve(__dirname, "../crowdin/messages.json"),
       JSON.stringify(messages_json),
       "utf8",
       (err) => console.log(err)
